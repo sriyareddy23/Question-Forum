292a7f9b38715a27ba5f565d7a5d21fa83a85b7e
Sriya Reddy <95168703+sriyareddy23@users.noreply.github.com>
2024-11-25 21:45:46 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,10 +8,6 @@ import commentRoutes from './routes/comments.js';
 import answerRoutes from './routes/answers.js'
 import tagRoutes from './routes/tags.js'
 import adminRoutes from './routes/admin.js'
-// import * as path from 'path';
-// import { fileURLToPath } from 'url';
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
 
 const app = express();
 
